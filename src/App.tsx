d4d66fc82444db988b387584d3180adc8feccca2
Gustavo Fernandes Vieira da Silva <gustavofernandesvs@gmail.com>
2025-10-12 02:10:17 -0300

    refactor: lazy load pages in App component and wrap Routes with Suspense for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from '@/pages/page'
-import Gallery from '@/pages/gallery'
-import Motivacao from '@/pages/motivacao'
 import { ThemeProvider } from "@/components/theme-provider"
 import TargetCursor from '@/components/ui/TargetCursor'
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('@/pages/page'))
+const Gallery = lazy(() => import('@/pages/gallery'))
+const Motivacao = lazy(() => import('@/pages/motivacao'))
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TargetCursor spinDuration={2} />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/motivacao" element={<Motivacao />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/motivacao" element={<Motivacao />} />
+          </Routes>
+        </Suspense>
       </Router>
     </ThemeProvider>
   )
