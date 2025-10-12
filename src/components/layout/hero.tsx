632914840d07775df15a75ac2315d8ff7cac2e53
Gustavo Fernandes Vieira da Silva <gustavofernandesvs@gmail.com>
2025-10-12 00:56:02 -0300

    feat: add GSAP for animations and implement ScrollFloat and TextPressure components

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -1,12 +1,21 @@
 import GetStartedButton from '@/components/ui/button-animated';
 import { Button } from '@/components/ui/button';
+import TextPressure from '@/components/ui/TextPressure'
 
 function Hero() {
   return (
       <main className='flex flex-col justify-center items-center flex-grow text-center'>
-        <h1 className='font-mono font-extrabold text-[90px] text-primary'>
-          AMAZONAS™
-        </h1>
+        <TextPressure
+            text="AMAZONAS™"
+            flex={true}
+            alpha={true}
+            stroke={false}
+            width={true}
+            weight={true}
+            italic={true}
+            minFontSize={36}
+            className='font-mono font-extrabold text-primary'
+        /> 
         <h2 className='font-mono text-secondary max-w-5xl'>
           Bem-vindo à Amazonas, onde a diversidade encontra a conveniência. Inspirados pela riqueza e pelo fluxo constante do maior rio do mundo, reunimos um catálogo 
           imenso de produtos para facilitar o seu dia a dia. Da tecnologia que move você aos itens que transformam sua casa, aqui você navega com facilidade, descobre novidades incríveis e recebe tudo com a agilidade que você merece.
